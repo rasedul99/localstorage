cb95a58d781b487d113ae28386a5ded7aee56176
rasedul99 <rasedul2199@gmail.com>
2022-03-22 12:32:29 +0600

    Multiple data in an object set to localStorage

--- src/utilities/localStorage.js
+++ src/utilities/localStorage.js
@@ -1,10 +1,31 @@
 const addToCart = (id) => {
-  const quantity = localStorage.getItem(id);
-  if (quantity) {
-    const newQuantity = parseInt(quantity) + 1;
-    localStorage.setItem(id, newQuantity);
+  // const quantity = localStorage.getItem(id);
+  // if (quantity) {
+  //   const newQuantity = parseInt(quantity) + 1;
+  //   localStorage.setItem(id, newQuantity);
+  // } else {
+  //   localStorage.setItem(id, 1);
+  // }
+
+  let shoppingCart;
+
+  // get the shoppingCart from localStorage
+  const storedCart = localStorage.getItem("shoping-cart");
+  if (storedCart) {
+    shoppingCart = JSON.parse(storedCart);
   } else {
-    localStorage.setItem(id, 1);
+    shoppingCart = {};
   }
+  // add quantity
+  const quantity = shoppingCart[id];
+
+  if (quantity) {
+    const newQuantity = quantity + 1;
+    shoppingCart[id] = newQuantity;
+  } else {
+    shoppingCart[id] = 1;
+  }
+  localStorage.setItem("shoping-cart", JSON.stringify(shoppingCart));
 };
+
 export { addToCart };
